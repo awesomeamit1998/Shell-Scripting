e0d5f1880dc0e1e6852a97102a0b70e75529eab2
Amit Jena <40141937+awesomeamit1998@users.noreply.github.com>
2019-06-16 20:33:56 +0530

    Add files via upload

---
+++ script2.sh
@@ -0,0 +1,7 @@
+echo "Total no of arguments is $#"
+echo "Script name is $0"
+echo "First argument is $1"
+echo "Second argument is $2"
+echo "Third argument is $3"
+echo "Total no of arguments is $@"
+echo "Total no of arguments together is $*"
